424f77a66be1d91df2e0cc20e8f5d20bc9d69265
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-20 16:21:02 +0430

    Add browserrouter in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
-function App() {}
+import { BrowserRouter } from 'react-router-dom';
+import Router from './route';
+
+const App = () => (
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+);
 
 export default App;
