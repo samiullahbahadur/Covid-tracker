20ed0baa2b32e319c33719fa850172c4fe3c48a6
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-20 14:41:03 +0430

    Setup project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+import MyComponent from "./MyComponent";
+function App() {
+  return <MyComponent title="React" />;
+}
+
+export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function MyComponent(props){
+
+    return(
+        <div>
+            <p>{props.title}</p>
+        </div>
+    )
+}
+
+export default MyComponent;
