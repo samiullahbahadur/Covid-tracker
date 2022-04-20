a6ba2fa2bee62d5949982fd75ead9c1077746fe3
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-20 17:27:22 +0430

    fix  search functionality

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,8 @@ import LandingImage from '../Layouts/LandingImage';
 import ItemsCard from './countryItem';
 
 const Home = () => {
- 
+  const [index, setState] = useState('');
+  const { countries } = useSelector((state) => state);
   return (
     <BasicLayoutContainer>
       <LandingImage />
